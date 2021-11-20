b4617d34c0ac777b9da5e3a5bcab458a8883012f
Moisés Brandão <moisesbrandao1998@gmail.com>
2021-11-20 08:36:20 -0300

    feat/ added api rul

--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const BASE_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'DEV_URL';
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:4000'
+    : 'https://gratibox-deltinha.herokuapp.com';
 
 export function postRegister(body) {
   return axios.post(`${BASE_URL}/register`, body);
